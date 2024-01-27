9427cac7f6663a50e5851433837bc2862c73b556
Siddhartha Khuntia <siddhartha.khuntia@lohum.com>
2024-01-27 16:30:23 +0530

    Add AWS Credentials
    Signed-off-by: Siddhartha Khuntia <siddhartha.khuntia@lohum.com>

--- aws-clients.js
+++ aws-clients.js
@@ -1,5 +1,16 @@
 import { TimestreamQueryClient } from "@aws-sdk/client-timestream-query";
+import { config } from "dotenv";
 
-const AWSregion = "us-east-1";
+config();
 
-export const queryClient = new TimestreamQueryClient({ region: AWSregion });
+const AWS_REGION = process.env.AWS_REGION;
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
+export const queryClient = new TimestreamQueryClient({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID,
+    secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  },
+});
